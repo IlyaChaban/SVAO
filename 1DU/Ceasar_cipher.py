da4aff96a130b4de38aa0004c280c0b3846604d2
IlyaChaban <ilyachaban@gmail.com>
2022-09-19 21:08:57 +0200

    Adding first home work in it's initial state

---
+++ 1DU/Ceasar_cipher.py
@@ -0,0 +1,11 @@
+MESSAGE = "do not eat all my oranges"
+TABLE = 'abcdefghijklmnopqrstuvwxyz '
+KEY = 1
+
+def caesar_encrypt(message, table, key):
+    # your code here
+    pass
+
+def caesar_decrypt(encrypted_message, table, key):
+    # your code here
+    pass
